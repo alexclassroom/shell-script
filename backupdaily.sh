ccacdc4bcbe7a2d67189fb88cccd2291b6dbed03
Alex Lion <learnwithalex@gmail.com>
2020-03-03 15:19:58 +0800

    Update backupdaily.sh

--- backupdaily.sh
+++ backupdaily.sh
@@ -3,4 +3,9 @@
 # Backup Necessary Configuration Files Daily
 # History:
 # 2020/03/03	Alex Lion	First release
-mkdir 
+
+# Create folder named as current date like mm-dd-yyyy under user home. For example, /home/alexlion/backup
+mkdir -p ~/backup/$(date +%x | tr '/' '-')
+
+# Create a log file and write current date information.
+date > ~/backup/$(date +%x | tr '/' '-')/$(date +%x | tr '/' '-')-backup.log
