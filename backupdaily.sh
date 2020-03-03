3090f85cb78b090ae772d59d4ff639d8d93c68d0
Alex Lion <learnwithalex@gmail.com>
2020-03-03 17:51:29 +0800

    Update backupdaily.sh

--- backupdaily.sh
+++ backupdaily.sh
@@ -5,7 +5,10 @@
 # 2020/03/03	Alex Lion	First release
 
 # Create folder named as current date like mm-dd-yyyy under user home. For example, /home/alexlion/backup
-mkdir -p ~/backup/$(date +%x | tr '/' '-')
+mkdir -p ~/backup/$(date +%Y%m%d)
 
 # Create a log file and write current date information.
-date > ~/backup/$(date +%x | tr '/' '-')/$(date +%x | tr '/' '-')-backup.log
+date > ~/backup/$(date +%Y%m%d)/backup-$(date +%Y%m%d).log
+
+# Log diak partition uasge
+df -h > ~/backup/$(date +%Y%m%d)/df-h-$(date +%Y%m%d).log
