3b2faabd46708f89b84e69155d33793847db5bf0
Alex Lion <learnwithalex@gmail.com>
2020-03-03 22:25:27 +0800

    Update dailybackup.sh

--- dailybackup.sh
+++ dailybackup.sh
@@ -4,14 +4,17 @@
 # History:
 # 2020/03/03	Alex Lion	First release
 
+#Declare Variables
+declare -i currentday=$(date +%Y%m%d)
+
 # Create folder named as current date like mm-dd-yyyy under user home. For example, /home/alexlion/backup
-mkdir -p ~/backup/$(date +%Y%m%d)
+mkdir -p ~/backup/$currentday
 
 # Create a log file and write current date information.
-date > ~/backup/$(date +%Y%m%d)/backup-$(date +%Y%m%d).log
+date > ~/backup/$currentday/backup-$currentday.log
 
 # If rc.local exists, copy it.
-test -e /etc/rc.local && cp /etc/rc.local ~/backup/$(date +%Y%m%d) && echo "rc.local Copyied" >> ~/backup/$(date +%Y%m%d)/backup-$(date +%Y%m%d).log || echo "rc.local Not Exist" >> ~/backup/$(date +%Y%m%d)/backup-$(date +%Y%m%d).log
+test -e /etc/rc.local && cp /etc/rc.local ~/backup/$currentday && echo "rc.local Copyied" >> ~/backup/$currentday/backup-$currentday.log || echo "rc.local Not Exist" >> ~/backup/$currentday/backup-$currentday.log
 
 # Log diak partition uasge
-date > ~/backup/$(date +%Y%m%d)/df-h-$(date +%Y%m%d).log && df -h >> ~/backup/$(date +%Y%m%d)/df-h-$(date +%Y%m%d).log
+date > ~/backup/$currentday/df-h-$currentday.log && df -h >> ~/backup/$currentday/df-h-$currentday.log
