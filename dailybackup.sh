c4b9257152b4619809aa1db79f6b92bc7476a1ee
Alex Lion <learnwithalex@gmail.com>
2020-03-04 00:32:15 +0800

    Update dailybackup.sh

--- dailybackup.sh
+++ dailybackup.sh
@@ -5,16 +5,16 @@
 # 2020/03/03	Alex Lion	First release
 
 # Declare Variables
-declare -i currentday=$(date +%Y%m%d)
+declare -i nowday=$(date +%Y%m%d)
 
-# Create folder named as current date like yyyymmdd under user home. For example, /home/alexlion/backup
-mkdir -p ~/backup/$currentday
+# Create folder named as current date like yyyymmdd under user home. For example, /home/alexlion/backup/20200303
+mkdir -p ~/backup/$nowday
 
 # Create a log file and write current date information.
-date > ~/backup/$currentday/backup-$currentday.log
+date > ~/backup/$nowday/backup-$nowday.log
 
 # If rc.local exists, copy it.
-test -e /etc/rc.local && cp /etc/rc.local ~/backup/$currentday && echo "rc.local Copyied" >> ~/backup/$currentday/backup-$currentday.log || echo "rc.local Not Exists" >> ~/backup/$currentday/backup-$currentday.log
+test -e /etc/rc.local && cp /etc/rc.local ~/backup/$nowday && echo "rc.local Copyied" >> ~/backup/$nowday/backup-$nowday.log || echo "rc.local Not Exists" >> ~/backup/$nowday/backup-$nowday.log
 
 # Log disk partition uasge
-date > ~/backup/$currentday/df-h-$currentday.log && df -h >> ~/backup/$currentday/df-h-$currentday.log
+date > ~/backup/$nowday/df-h-$nowday.log && df -h >> ~/backup/$nowday/df-h-$nowday.log
