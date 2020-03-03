69f452e76b78742fcca2a776402363fd8664da9f
Alex Lion <learnwithalex@gmail.com>
2020-03-04 01:05:07 +0800

    Update runbarryrun.sh

--- runbarryrun.sh
+++ runbarryrun.sh
@@ -4,16 +4,16 @@
 # History:
 # 2020/03/03	Alex Lion	First release
 
-# Declare Variables
-declare -i currentday=$(date +%Y%m%d);
-
 #Get sh file from GitHub public repository
 wget https://raw.githubusercontent.com/alexclassroom/shell-script/master/dailybackup.sh
 
 #After file downloading is done, wait for 3 seconds.
 sleep 3
 
-# Check dailybackup.sh eists or not. If file exists, run it.
+# Declare Variables
+declare -i currentday=$(date +%Y%m%d);
+
+# Check dailybackup.sh exists or not. If file exists, run it.
 test -e ~/dailybackup.sh && sh dailybackup.sh || echo "dailybackup.sh Not Exists, $currentday backup failed." > Backup-Failed-$currentday.log
 
 #After process is done, wait for 1 second.
