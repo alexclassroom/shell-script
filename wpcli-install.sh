2b8458091dad48d4e9bdcd4f505a32b7c08ed8a4
Alex Lion <learnwithalex@gmail.com>
2021-01-17 01:10:00 -0800

    Add Comment

--- wpcli-install.sh
+++ wpcli-install.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+# Program:
+# This script can install the latest WP-CLI for your environment.
+# History:
+# 2020/03/03	Alex Lion	First release
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && sudo mv wp-cli.phar /usr/local/bin/wp
